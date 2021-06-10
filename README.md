9dfcf21e3935b322ba4c4a6a9a4be926e13953d7
Hemanth N <hemanthnov2001@gmail.com>
2021-06-11 00:10:36 +0530

    Added source files for the template

---
+++ README.md
@@ -0,0 +1,9 @@
+# REACT TAILWIND TEMPLATE
+
+- Starter template for a react project with tailwind framework.
+- Clone the project - run npm install and start editing in src files. Tailwind is setup
+
+
+### Followed the documention for the setup.
+
+###### Link to be added.
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-center">
+ 
+    <h1 className="text-4xl font-bold pt-20">Insert whatever, Start a Project - Useable Template</h1>
+    <h1 className="text-3xl font-bold pt-20">React + Tailwind setup is done, This cud be used as a template for future projects</h1>
+    <a href="https://github.com/hemanth-07-11" target="_blank" rel="noreferrer" className="text-blue-500 underline pt-20">Follow me on github</a>
+    </div>
+  );
+}
+
+export default App;
